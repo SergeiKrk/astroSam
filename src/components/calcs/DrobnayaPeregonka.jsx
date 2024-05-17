72bb9cb7b7c7c5dd85a4ba42ce15d6cf96c00d43
SergeiKrk <krjukov_s@mail.ru>
2024-05-17 23:14:32 +0400

    поправил работу DrobnayaPeregonka

--- src/components/calcs/DrobnayaPeregonka.jsx
+++ src/components/calcs/DrobnayaPeregonka.jsx
@@ -1,47 +1,26 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const FractionalDistillationCalculator = () => {
-	const [rawAlcoholVol, setRawAlcoholVol] = useState('')
-	const [rawAlcoholFortr, setRawAlcoholFortr] = useState('')
-	const [afterDistillingFortr, setAfterDistillingFortr] = useState('')
-	const [partHeadVol, setPartHeadVol] = useState(10) // Default value set to 10%
-	const [partTailVol, setPartTailVol] = useState(10) // Default value set to 10%
+	const rawAlcoholVolRef = useRef(null)
+	const rawAlcoholFortrRef = useRef(null)
+	const afterDistillingFortrRef = useRef(null)
+	const partHeadRef = useRef(null)
+	const partTailRef = useRef(null)
 
-	const handleInputChange = (e) => {
-		const { id, value } = e.target
-		switch (id) {
-			case 'rawAlcoholVol':
-				setRawAlcoholVol(value)
-				break
-			case 'rawAlcoholFortr':
-				setRawAlcoholFortr(value)
-				break
-			case 'afterDistillingFortr':
-				setAfterDistillingFortr(value)
-				break
-			case 'partHead':
-				setPartHeadVol(value)
-				break
-			case 'partTail':
-				setPartTailVol(value)
-				break
-			default:
-				break
-		}
-		calculate()
-	}
+	const [absAlcoholValue, setRawAlcoholVol] = useState(0)
+	const [outHeadsValue, setOutHeadslVol] = useState(0)
+	const [outTailsValue, setOutTailsVol] = useState(0)
+	const [reqVolValue, setReqVol] = useState(0)
 
-	const calculate = () => {
-		let rawAlcoholVolValue = parseFloat(rawAlcoholVol)
-		let rawAlcoholFortrValue = parseFloat(rawAlcoholFortr)
-		let afterDistillingFortrValue = parseFloat(afterDistillingFortr)
-		let partHeadVolValue = parseFloat(partHeadVol)
-		let partTailVolValue = parseFloat(partTailVol)
+	const calculateDrobnayaPeregonka = () => {
+		let rawAlcoholVolValue = parseFloat(rawAlcoholVolRef.current.value)
+		let rawAlcoholFortrValue = parseFloat(rawAlcoholFortrRef.current.value)
+		let afterDistillingFortrValue = parseFloat(afterDistillingFortrRef.current.value)
+		let partHeadVolValue = parseFloat(partHeadRef.current.value)
+		let partTailVolValue = parseFloat(partTailRef.current.value)
 
-		let absAlcoholValue = ((rawAlcoholFortrValue * rawAlcoholVolValue) / 100).toFixed(2)
-		if (isNaN(absAlcoholValue)) {
-			absAlcoholValue = 0
-		}
+		let absAlcohol = ((rawAlcoholFortrValue * rawAlcoholVolValue) / 100).toFixed(2)
+
 		let outHeadsValue = (
 			(rawAlcoholFortrValue * rawAlcoholVolValue * partHeadVolValue) /
 			10000
@@ -56,112 +35,118 @@ const FractionalDistillationCalculator = () => {
 			outTailsValue
 		).toFixed(2)
 
-		if (isNaN(outHeadsValue) || isNaN(outTailsValue) || isNaN(reqVolValue)) {
-			outHeadsValue = 0
-			outTailsValue = 0
-			reqVolValue = 0
-		}
-
-		document.getElementById('absAlcohol').innerText =
-			`Абсолютный объем спирта: ${absAlcoholValue} л`
-		document.getElementById('outHeads').innerText = `Объем «Голов»: ${outHeadsValue} л`
-		document.getElementById('outTails').innerText = `Объем «Хвостов»: ${outTailsValue} л`
-		document.getElementById('reqVol').innerText = `Необходимый объем: ${reqVolValue} л`
+		rawAlcoholFortrValue && rawAlcoholVolValue && setRawAlcoholVol(absAlcohol)
+		rawAlcoholFortrValue && rawAlcoholVolValue && setOutHeadslVol(outHeadsValue)
+		rawAlcoholFortrValue && rawAlcoholVolValue && setOutTailsVol(outTailsValue)
+		rawAlcoholFortrValue &&
+			rawAlcoholVolValue &&
+			afterDistillingFortrValue &&
+			setReqVol(reqVolValue)
 	}
 
 	return (
-		<div className='container'>
-			<div className='card-group'>
-				<div className='card border border-dark'>
-					<div className='card-body'>
-						<div className='card-text'>
-							<form>
-								<div className='form-group'>
-									<label htmlFor='rawAlcoholVol'>Объем спирта-сырца, литры:</label>
-									<input
-										type='number'
-										className='form-control'
-										id='rawAlcoholVol'
-										placeholder='начальный объем, литров'
-										value={rawAlcoholVol}
-										onChange={handleInputChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='rawAlcoholFortr'>Крепость спирта-сырца, градусов:</label>
-									<input
-										type='number'
-										className='form-control'
-										id='rawAlcoholFortr'
-										placeholder='начальная крепость, градусов'
-										value={rawAlcoholFortr}
-										onChange={handleInputChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='afterDistillingFortr'>
-										Нужная крепость после перегона, градусов:
-									</label>
-									<input
-										type='number'
-										className='form-control'
-										id='afterDistillingFortr'
-										placeholder='требуемая крепость, градусов'
-										value={afterDistillingFortr}
-										onChange={handleInputChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='partHead'>Доля «Голов», % от общего объема:</label>
-									<select
-										className='form-control'
-										id='partHead'
-										value={partHeadVol}
-										onChange={handleInputChange}
-									>
-										<option value='10'>10%</option>
-										<option value='11'>11%</option>
-										<option value='12'>12%</option>
-										<option value='13'>13%</option>
-										<option value='14'>14%</option>
-										<option value='15'>15%</option>
-									</select>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='partTail'>Доля «Хвостов», % от общего объема:</label>
-									<select
-										className='form-control'
-										id='partTail'
-										value={partTailVol}
-										onChange={handleInputChange}
-									>
-										<option value='10'>10%</option>
-										<option value='11'>11%</option>
-										<option value='12'>12%</option>
-										<option value='13'>13%</option>
-										<option value='14'>14%</option>
-										<option value='15'>15%</option>
-										<option value='16'>16%</option>
-										<option value='17'>17%</option>
-										<option value='18'>18%</option>
-										<option value='19'>19%</option>
-										<option value='20'>20%</option>
-									</select>
-								</div>
-							</form>
-						</div>
+		<div className='w-full flex flex-col lg:flex-row'>
+			<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
+				<form className='mx-6 my-6 lg:my-16 text-center'>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='p10 mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='rawAlcoholVol'>
+							Объем спирта-сырца:
+						</label>
+						<input
+							type='number'
+							className='w-full lg:w-[30%] border-2 rounded-lg border-[#1ABC9C] px-1 text-center lg:text-left'
+							ref={rawAlcoholVolRef}
+							placeholder='0 л.'
+							onChange={calculateDrobnayaPeregonka}
+						/>
 					</div>
-				</div>
-				<div className='card border-dark bg-primary text-center'>
-					<blockquote className='blockquote mb-0 card-body'>
-						<p id='absAlcohol'>Абсолютный объем спирта: 0 л</p>
-						<p id='outHeads'>Объем «Голов»: 0 л</p>
-						<p id='outTails'>Объем «Хвостов»: 0 л</p>
-						<p id='reqVol'>Необходимый объем: 0 л</p>
-					</blockquote>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='p10 mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='rawAlcoholFortr'>
+							Крепость спирта-сырца:
+						</label>
+						<input
+							type='number'
+							className='w-full lg:w-[30%] border-2 rounded-lg border-[#1ABC9C] px-1 text-center lg:text-left'
+							ref={rawAlcoholFortrRef}
+							placeholder='0 °'
+							onChange={calculateDrobnayaPeregonka}
+						/>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label
+							className='p10 mr-4 lg:text-right lg:w-[70%] w-full'
+							htmlFor='afterDistillingFortr'
+						>
+							Нужная крепость после перегона:
+						</label>
+						<input
+							type='number'
+							className='w-full lg:w-[30%] border-2 rounded-lg border-[#1ABC9C] px-1 text-center lg:text-left'
+							id='afterDistillingFortr'
+							ref={afterDistillingFortrRef}
+							placeholder='0 °'
+							onChange={calculateDrobnayaPeregonka}
+						/>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='p10 mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='partHead'>
+							Доля «Голов»:
+						</label>
+						<select
+							className='w-full lg:w-[30%] border-2 rounded-lg border-[#1ABC9C] px-1 text-center lg:text-left'
+							id='partHead'
+							ref={partHeadRef}
+							onChange={calculateDrobnayaPeregonka}
+						>
+							<option value='10'>10%</option>
+							<option value='3'>3%</option>
+							<option value='7'>7%</option>
+							<option value='11'>11%</option>
+							<option value='12'>12%</option>
+							<option value='13'>13%</option>
+							<option value='14'>14%</option>
+							<option value='15'>15%</option>
+						</select>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='p10 mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='partTail'>
+							Доля «Хвостов»:
+						</label>
+						<select
+							className='w-full lg:w-[30%] border-2 rounded-lg border-[#1ABC9C] px-1 text-center lg:text-left'
+							id='partTail'
+							ref={partTailRef}
+							onChange={calculateDrobnayaPeregonka}
+						>
+							<option value='10'>10%</option>
+							<option value='15'>11%</option>
+							<option value='20'>12%</option>
+							<option value='25'>13%</option>
+							<option value='30'>14%</option>
+						</select>
+					</div>
+				</form>
+			</div>
+			<div className='basis-1/2 content-center border-4 lg:rounded-r-lg border-[#1ABC9C] dark:border-[#00614B] bg-[#1ABC9C] dark:bg-[#00614B] text-center'>
+				<div className='mb-0 py-6 text-xl text-white'>
+					<p>Объем абсолютного спирта:</p>
+					<p>
+						<span className='text-4xl'>{absAlcoholValue}</span> л
+					</p>
+					<p>Объем «Голов»:</p>
+					<p>
+						<span className='text-4xl'>{outHeadsValue}</span> л
+					</p>
+					<p>Объем «Хвостов»:</p>
+					<p>
+						<span className='text-4xl'>{outTailsValue}</span> л
+					</p>
+					<p>Объем продукта на выходе:</p>
+					<p>
+						<span className='text-4xl'>{reqVolValue}</span> л
+					</p>
 				</div>
 			</div>
-			<br />
 		</div>
 	)
 }
