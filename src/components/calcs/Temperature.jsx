e4063e6d0c1d503f2dad13e0103e35809224720b
SergeiKrk <krjukov_s@mail.ru>
2024-05-19 10:29:47 +0400

    исправил калькулятор Temperature

--- src/components/calcs/Temperature.jsx
+++ src/components/calcs/Temperature.jsx
@@ -22,53 +22,45 @@ const TemperatureCalculator = () => {
 	}
 
 	return (
-		<div className='container mx-auto'>
-			<div className='w-full flex flex-col lg:flex-row'>
-				<div className='card border border-gray-300'>
-					<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
-						<h5 className='card-title'>Введите данные для расчета:</h5>
-						<div className='card-text'>
-							<br />
-							<div className='my-2 flex flex-wrap lg:flex-nowrap'>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='firstStr'>
-										Крепость по спиртометру:
-									</label>
-									<input
-										ref={firstStrRef}
-										id='firstStr'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='градусов'
-										onChange={calculateTemperature}
-									/>
-									<div className='col-span-1 p-2'>, °</div>
-								</div>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='firstTemp'>
-										Температура спирта:
-									</label>
-									<input
-										ref={firstTempRef}
-										id='firstTemp'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='20 °C'
-										onChange={calculateTemperature}
-									/>
-									<div className='col-span-1 p-2'>, °C.</div>
-								</div>
-							</div>
-						</div>
+		<div className='w-full flex flex-col lg:flex-row'>
+			<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
+				<form className='m-6 text-center'>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='firstStr'>
+							Крепость по спиртометру:
+						</label>
+						<input
+							ref={firstStrRef}
+							id='firstStr'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='0°'
+							onChange={calculateTemperature}
+						/>
 					</div>
-				</div>
-				<div className='card border border-gray-300 bg-primary text-center'>
-					<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
-						<p>Крепость: {strength}°</p>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='firstTemp'>
+							Температура спирта:
+						</label>
+						<input
+							ref={firstTempRef}
+							id='firstTemp'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='20 °C'
+							onChange={calculateTemperature}
+						/>
 					</div>
+				</form>
+			</div>
+			<div className='basis-1/2 content-center border-4 lg:rounded-r-lg border-[#1ABC9C] dark:border-[#00614B] bg-[#1ABC9C] dark:bg-[#00614B] text-center'>
+				<div className='mb-0 py-6 text-xl text-white'>
+					<p>Крепость:</p>
+					<p>
+						<span className='text-4xl'>{strength}</span>°
+					</p>
 				</div>
 			</div>
-			<br />
 		</div>
 	)
 }
