59728cbad1d6f1c989ccc74533c1831413441d37
SergeiKrk <krjukov_s@mail.ru>
2024-05-19 09:07:43 +0400

    исправил калькулятор SebestoimApprox

--- src/components/calcs/Sebestoimost.jsx
+++ src/components/calcs/Sebestoimost.jsx
@@ -41,127 +41,112 @@ const SebestoimostCalc = () => {
 	}
 
 	return (
-		<div className='container mx-auto'>
-			<div className='w-full flex flex-col lg:flex-row'>
-				<div className='card border border-gray-300'>
-					<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
-						<div className='card-text'>
-							<br />
-							<div className='my-2 flex flex-wrap lg:flex-nowrap'>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='materialCost'>
-										Цена сырья за 1 кг.:
-									</label>
-									<input
-										ref={materialCostRef}
-										id='materialCost'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='цена за 1 кг.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, руб.</div>
-								</div>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='materialMass'>
-										Масса сырья:
-									</label>
-									<input
-										ref={materialMassRef}
-										id='materialMass'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='масса, кг.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, кг.</div>
-								</div>
-								<br />
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='yeastCost'>
-										Стоимость дрожжей:
-									</label>
-									<input
-										ref={yeastCostRef}
-										id='yeastCost'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='цена, руб.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, руб.</div>
-								</div>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='bentoniteCost'>
-										Стоимость бентонита:
-									</label>
-									<input
-										ref={bentoniteCostRef}
-										id='bentoniteCost'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='стоимость, руб.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, руб.</div>
-								</div>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='coalCost'>
-										Стоимость угля:
-									</label>
-									<input
-										ref={coalCostRef}
-										id='coalCost'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='стоимость, руб.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, руб.</div>
-								</div>
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='wgeCost'>
-										Вода, газ / электричество:
-									</label>
-									<input
-										ref={wgeCostRef}
-										id='wgeCost'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='стоимость, руб.'
-										onChange={allValuesOnChange}
-									/>
-									<div className='col-span-1 p-2'>, руб.</div>
-								</div>
-								<br />
-								<div className='grid grid-cols-2 gap-4'>
-									<label className='text-right p-2' htmlFor='volumeDrink'>
-										Объем готового напитка:
-									</label>
-									<input
-										ref={volumeDrinkRef}
-										id='volumeDrink'
-										className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
-										type='number'
-										placeholder='Объем напитка, л.'
-										onChange={(e) => setVolumeDrink(e.target.value)}
-									/>
-									<div className='col-span-1 p-2'>, л.</div>
-								</div>
-							</div>
-						</div>
+		<div className='w-full flex flex-col lg:flex-row'>
+			<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
+				<form className='m-6 text-center'>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='materialCost'>
+							Цена сырья за 1 кг.:
+						</label>
+						<input
+							ref={materialCostRef}
+							id='materialCost'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='цена за 1 кг.'
+							onChange={allValuesOnChange}
+						/>
 					</div>
-				</div>
-				<div className='card border border-gray-300 bg-primary text-center'>
-					<div className='basis-1/2 border-4 lg:rounded-l-lg border-[#1ABC9C] dark:border-[#00614B]'>
-						<p>Стоимость алкоголя (за литр): {costLiter} ₽</p>
-						<p>Стоимость алкоголя (за пол-литра): {costHalfLiter} ₽</p>
-						<p>Объем абсолютного спирта: {absoluteAlcoholVolume} л</p>
-						<p>Объем алкоголя питейной крепости: {volumeDrink} л</p>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='materialMass'>
+							Масса сырья:
+						</label>
+						<input
+							ref={materialMassRef}
+							id='materialMass'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='масса, кг.'
+							onChange={allValuesOnChange}
+						/>
 					</div>
+					<br />
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='yeastCost'>
+							Стоимость дрожжей:
+						</label>
+						<input
+							ref={yeastCostRef}
+							id='yeastCost'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='цена, руб.'
+							onChange={allValuesOnChange}
+						/>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='bentoniteCost'>
+							Стоимость бентонита:
+						</label>
+						<input
+							ref={bentoniteCostRef}
+							id='bentoniteCost'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='стоимость, руб.'
+							onChange={allValuesOnChange}
+						/>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='coalCost'>
+							Стоимость угля:
+						</label>
+						<input
+							ref={coalCostRef}
+							id='coalCost'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='стоимость, руб.'
+							onChange={allValuesOnChange}
+						/>
+					</div>
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='wgeCost'>
+							Вода, газ / электричество:
+						</label>
+						<input
+							ref={wgeCostRef}
+							id='wgeCost'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='стоимость, руб.'
+							onChange={allValuesOnChange}
+						/>
+					</div>
+					<br />
+					<div className='my-2 flex flex-wrap lg:flex-nowrap'>
+						<label className='mr-4 lg:text-right lg:w-[70%] w-full' htmlFor='volumeDrink'>
+							Объем готового напитка:
+						</label>
+						<input
+							ref={volumeDrinkRef}
+							id='volumeDrink'
+							className='w-full lg:w-[30%] bg-white text-black border-2 rounded-lg border-[#1ABC9C] px-1 max-w-52 text-center lg:text-left col-span-1'
+							type='number'
+							placeholder='Объем напитка, л.'
+							onChange={(e) => setVolumeDrink(e.target.value)}
+						/>
+					</div>
+				</form>
+			</div>
+			<div className='basis-1/2 content-center border-4 lg:rounded-r-lg border-[#1ABC9C] dark:border-[#00614B] bg-[#1ABC9C] dark:bg-[#00614B] text-center'>
+				<div className='mb-0 py-6 text-xl text-white'>
+					<p>Стоимость алкоголя (за литр): {costLiter} ₽</p>
+					<p>Стоимость алкоголя (за пол-литра): {costHalfLiter} ₽</p>
+					<p>Объем абсолютного спирта: {absoluteAlcoholVolume} л</p>
+					<p>Объем алкоголя питейной крепости: {volumeDrink} л</p>
 				</div>
 			</div>
-			<br />
 		</div>
 	)
 }
